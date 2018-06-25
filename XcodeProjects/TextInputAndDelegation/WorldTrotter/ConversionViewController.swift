942a1a12b32a53ad973e107bfa289f505672b078
Benjamin Morales <ing.benjamin.morales@gmail.com>
2018-06-24 23:45:39 -0500

    feat: Add TextInputAnddelegation projecto to homeworks

---
+++ XcodeProjects/TextInputAndDelegation/WorldTrotter/ConversionViewController.swift
@@ -0,0 +1,75 @@
+//
+//  ConversionViewController.swift
+//  WorldTrotter
+//
+//  Created by Benjamin Morales on 24/06/18.
+//  Copyright © 2018 Benjamin Morales. All rights reserved.
+//
+
+import UIKit
+
+class ConversionViewController: UIViewController, UITextFieldDelegate {
+    
+    @IBOutlet weak var celsiusLabel: UILabel!
+    
+    @IBOutlet weak var textField: UITextField!
+    
+    var fahrenheitValue: Measurement<UnitTemperature>? {
+        didSet {
+            updateCelsiusValue()
+        }
+    }
+    
+    var celsiusValue: Measurement<UnitTemperature>? {
+        if let fahrenheitValue = fahrenheitValue {
+            return fahrenheitValue.converted(to: .celsius)
+        } else {
+            return nil
+        }
+    }
+    
+    let numberFormatter: NumberFormatter = {
+        let nf = NumberFormatter()
+        nf.numberStyle = .decimal
+        nf.minimumFractionDigits = 0
+        nf.maximumFractionDigits = 1
+        return nf }()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        updateCelsiusValue()
+    }
+    
+    @IBAction func fahrenheitFieldEditingChanged(_ textField: UITextField) {
+        if let text = textField.text, let value = Double(text) {
+            fahrenheitValue = Measurement(value: value, unit: .fahrenheit)
+        } else {
+            fahrenheitValue = nil
+        }
+    }
+    
+    @IBAction func dismissKeyboard(_ sender: UITapGestureRecognizer) {
+        textField.resignFirstResponder()
+    }
+    
+    func updateCelsiusValue() {
+        if let celsiusValue = celsiusValue {
+            celsiusLabel.text = numberFormatter.string(from: NSNumber(value: celsiusValue.value))
+        } else {
+            celsiusLabel.text = "???"
+        }
+    }
+    
+    func textField(_ textField: UITextField, shouldChangeCharactersIn range: NSRange, replacementString string: String) -> Bool {
+        let existingTextHasDecimalSeparator = textField.text?.range(of: ".")
+        let replacementTextHasDecimalSeparator = string.range(of: ".")
+        if existingTextHasDecimalSeparator != nil,
+            replacementTextHasDecimalSeparator != nil {
+            return false
+        } else {
+            return true
+        }
+    }
+    
+}
+
