4fb9e0a7318323ea115d21e17966987de9ccc417
Benjamin Morales <ing.benjamin.morales@gmail.com>
2018-06-25 01:23:29 -0500

    feat: Add ViewControllers project to Homework

---
+++ XcodeProjects/ViewControllers/WorldTrotter/MapViewController.swift
@@ -0,0 +1,18 @@
+//
+//  MapViewController.swift
+//  WorldTrotter
+//
+//  Created by Benjamin Morales on 25/06/18.
+//  Copyright © 2018 Benjamin Morales. All rights reserved.
+//
+
+import UIKit
+
+class MapViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        print("MapViewController loaded its view.")
+    }
+
+}
